40ffba60502bd3c730a20bbe00601021d4191742
Adrien Beudin <beudbeud@gmail.com>
2025-06-17 15:37:04 +0200

    feat: make mouse with 5 buttons

--- Arduino_ArcadeSpinner.ino
+++ Arduino_ArcadeSpinner.ino
@@ -1,10 +1,11 @@
 /*  
  *  Arduino USB Arcade Spinner
- *  (C) Wilfried JEANNIARD [https://github.com/willoucom]
+ *  (C) Adrien Beudin [https://github.com/beudbeud]
  *  
  *  Based on project by Alexey Melnikov [https://github.com/MiSTer-devel/Retro-Controllers-USB-MiSTer/blob/master/PaddleTwoControllersUSB/PaddleTwoControllersUSB.ino]
  *  Based on project by Mikael Norrgård <mick@daemonbite.com>
- *  
+ *  Based on project [https://github.com/willoucom/Arduino_ArcadeSpinner]
+ *
  *  GNU GENERAL PUBLIC LICENSE
  *  Version 3, 29 June 2007
  *  
@@ -24,17 +25,14 @@
  */
 
 ///////////////// Customizable settings /////////////////////////
-// For debug (check serial monitor)
-// #define DEBUG
-
 // Spinner pulses per revolution
 #define SPINNER_PPR 600
 
 // Spinner/Mouse sensitivity
 // 1 is more sensitive 
 // 999 is less sensitive
-#define SPINNER_SENSITIVITY 15
-#define MOUSE_SENSITIVITY 1
+#define SPINNER_SENSITIVITY 500
+#define MOUSE_SENSITIVITY 5
 
 /////////////////////////////////////////////////////////////////
 
@@ -44,22 +42,13 @@
 // Pins used by buttons
 #define Button0 5 // Left button
 #define Button1 4 // Right button
-#define Button2 15 // Small Left: Select/Coin
-#define Button3 14 // Small Right: Start
+#define Button2 14 // Middle
+#define Button3 15 // Middle
+#define Button4 6 // Middle
 
 ////////////////////////////////////////////////////////
 
-// ID for special support in MiSTer 
-// ATT: 20 chars max (including NULL at the end) according to Arduino source code.
-// Additionally serial number is used to differentiate arduino projects to have different button maps!
-const char *gp_serial = "MiSTer-A1 Spinner";
-
-#include <Mouse.h>
-#include "Gamepad.h"
-
-// Create Gamepad
-Gamepad_ Gamepad;
-GamepadReport rep;
+#include "HID-Project.h"
 
 // Default virtual spinner position
 int16_t drvpos = 0;
@@ -72,10 +61,8 @@ int16_t m_drvpos = 0;
 #define SP_MAX ((SPINNER_PPR*4*270UL)/360)
 int32_t sp_clamp = SP_MAX/2;
 
-// For emulation
-bool mouse_emu = 0;
-bool paddle_emu = 0;
-bool mr_spinner_emu = 0;
+// Response delay of the mouse, in ms
+int responseDelay = 10;
 
 // Interrupt pins of Rotary Encoder
 void drv_proc()
@@ -106,11 +93,6 @@ void drv_proc()
 // Run at startup
 void setup()
 {
-  #ifdef DEBUG
-    Serial.begin(9600);
-  #endif
-  Gamepad.reset();
-
   // Encoder
   pinMode(pinA, INPUT_PULLUP);
   pinMode(pinB, INPUT_PULLUP);
@@ -125,82 +107,42 @@ void setup()
   pinMode(Button1, INPUT_PULLUP);
   pinMode(Button2, INPUT_PULLUP);
   pinMode(Button3, INPUT_PULLUP);
-
-  // Enable mouse emulation
-  if (!digitalRead(Button0)) {
-    mouse_emu = !mouse_emu;
-    Mouse.begin();
-  } 
-  // Enable paddle emulation
-  if (!digitalRead(Button1)) {
-    paddle_emu = !paddle_emu;
-  }
-  // Spinner only (AKA mr.Spinner mode)
-  if (!digitalRead(Button2)) {
-    // Announce the device as mr.Spinner (more explanations in the readme file)
-    mr_spinner_emu = !mr_spinner_emu;
-    gp_serial = "MiSTer-S1 Spinner";
-  }
-  
+  pinMode(Button4, INPUT_PULLUP);
 }
 
 // Main loop
 void loop()
 {
-  // Default Spinner/Paddle position;
-  rep.paddle = 0;
-  rep.spinner = 0;
-  // Buttons
-  if (mr_spinner_emu) {
-    rep.b0 = !digitalRead(Button0) || !digitalRead(Button1) || !digitalRead(Button2) || !digitalRead(Button3);
-  } else {
-    rep.b0 = !digitalRead(Button0);
-    rep.b1 = !digitalRead(Button1);
-    rep.b2 = !digitalRead(Button2);
-    rep.b3 = !digitalRead(Button3);
-    }
 
-  // spinner rotation
-  static uint16_t prev = 0;
-  int16_t val = ((int16_t)(drvpos - prev));
-  if(val>127) val = 127; else if(val<-127) val = -127;
-  rep.spinner = val;
-  prev += val;
-
-  // Paddle Emulation
-  if(paddle_emu) {
-    rep.paddle = ((sp_clamp*255)/SP_MAX);
-    rep.spinner = 0;
-  }
-
-  // Mouse Emulation
-  if(mouse_emu) {
-    static uint16_t m_prev = 0;
-    int16_t val = ((int16_t)(m_drvpos - m_prev));
-    if(val>127) val = 127; else if(val<-127) val = -127;
-    m_prev += val;
-    Mouse.move(val, 0);
-    rep.spinner = 0;
-  }
-
-  // Only report controller state if it has changed
-  if (memcmp(&Gamepad._GamepadReport, &rep, sizeof(GamepadReport)))
+  struct ButtonMapper
   {
-    #ifdef DEBUG
-      // Very verbose debug
-      Serial.print(gp_serial); Serial.print(" ");
-      Serial.print(drvpos); Serial.print(" ");
-      Serial.print(mouse_emu); Serial.print(" ");
-      Serial.print(paddle_emu); Serial.print(" ");
-      Serial.print(rep.spinner); Serial.print(" ");
-      Serial.print(rep.paddle); Serial.print(" ");
-      Serial.print(rep.b0); Serial.print(" ");
-      Serial.print(rep.b1); Serial.print(" ");
-      Serial.print(rep.b2); Serial.print(" ");
-      Serial.println(rep.b3);
-    #endif
-    // Send Gamepad changes
-    Gamepad._GamepadReport = rep;
-    Gamepad.send();
+    int mButton;
+    int mMouseButton;
+  };
+
+  static const ButtonMapper buttonsToMouseButtons[] =
+  {
+    { Button0, MOUSE_LEFT },
+    { Button1, MOUSE_RIGHT },
+    { Button2, MOUSE_MIDDLE },
+    { Button3, MOUSE_PREV },
+    { Button4, MOUSE_NEXT },
+  };
+
+  for(const ButtonMapper& mapper : buttonsToMouseButtons)
+  {
+    if (!digitalRead(mapper.mButton) == HIGH)
+    {
+      if (!Mouse.isPressed(mapper.mMouseButton)) Mouse.press(mapper.mMouseButton);
+    }
+    else if (Mouse.isPressed(mapper.mMouseButton)) Mouse.release(mapper.mMouseButton);
   }
+
+  delay(responseDelay);
+
+  static uint16_t m_prev = 0;
+  int16_t val = ((int16_t)(m_drvpos - m_prev));
+  if(val>127) val = 127; else if(val<-127) val = -127;
+  m_prev += val;
+  Mouse.move(val, 0);
 }
--- README.md
+++ README.md
@@ -4,78 +4,9 @@ This project is a modern re-creation of the spinners used in arcades, mainly for
 
 The objective is to offer a feeling close to the original equipment by using components that are easy to obtain.
 
-![Img!](img/pic.jpg "Arcade Spinner")
-
-## In action
-
-### Arkanoid with rotary controller
-[![IMAGE ALT TEXT](http://img.youtube.com/vi/QnDsVMLP6zM/0.jpg)](http://www.youtube.com/watch?v=QnDsVMLP6zM "Arkanoid with rotary controller on MisterFPGA")
-
-### Puzz'Loop 2 with rotary controller
-[![IMAGE ALT TEXT](http://img.youtube.com/vi/TzKd8ytODUM/0.jpg)](http://www.youtube.com/watch?v=TzKd8ytODUM "Puzz'Loop 2 with rotary controller on MisterFPGA")
-
-### Eco Fighter / Ultimate Ecology with rotary controller
-[![IMAGE ALT TEXT](http://img.youtube.com/vi/sb1mO-uq1ag/0.jpg)](http://www.youtube.com/watch?v=sb1mO-uq1ag "Eco Fighter / Ultimate Ecology with rotary controller on MisterFPGA")
-
-
-
 ## Usage
 
-By default the device operate as a 4 button controller with Spinner/Paddle attached
-
-You can activate an additional behavior by holding a button when you plug in the device : 
-
-- Press Button0 (Big Left button) to enable MouseX Emulation
-- Press Button1 (Big Right button) to enable Paddle Emulation
-- Press Button2 (Small Left button) to switch device to mr.Spinner (enabling composite device on mister fpga)
- - In mr.Spinner mode, each button press is redirected to button 1
-
-## MisterFPGA
-
-### Working
-
-| Game                          | Note|
-|---                            |--- | 
-| Arkanoid (World)              | |
-| Block Gal                     | |
-| Breakout (TTL)                | Enable spinner in controls|
-| Crater Raider                 | |
-| Dark Planet                   | |
-| Demolition Derby              | |
-| Discs of Tron                 | |
-| Eco Fighters                  | Use mr.Spinner mode |
-| Forgotten Worlds              | Use mr.Spinner mode |
-| Kick                          | |
-| Kozmik Krooz'r                | |
-| Midnight Resistance           | Use mr.Spinner mode |
-| Moonwar                       | |
-| Pong                          | Use Paddle emulation and set your player to Paddle |
-| Pop'n Bounce (NeoGeo)         | Enable Paddle in Dip settings (Soft dip->Pop'n bounce->Controller = paddle) |
-| Puzz Loop 2                   | Enable Paddle in Service mode (System -> Operation) |
-| Tron                          | |
-| Two Tigers (tron conversion)  | |
-
-### Working with workaround
-| Game                  | Note|
-|---                    |---| 
-| Block block (world)   | Use alternatives/block block (world 910910), must use Mouse Emulation |
-| Super Breakout        | Enable paddle in controls, must use paddle emulation |
-
-## Mame
-
-You can use the controller with mame and derivative by using the controller in Mouse Emulation mode.
-
-> Note: You can define the default behavior of the controller by modifying the source code, to do this modify the default value of the variable `bool mouse_emu = 0;` to 1t which will have the effect of operating the controller in mouse mode
-
-You need to change the `dial device assignment` option in the mame settings either in mame.ini or using the GUI.
-
-### Edit mame.ini 
-```
-dial_device               mouse
-```
-### Using MAME GUI
-
-![Img!](img/mame.jpg "MAME")
+By default the device operate as a 5 button controller with Spinner
 
 ## Build yourself
 
@@ -84,7 +15,7 @@ dial_device               mouse
 - Rotary encoder (Model: C38S6G5-600B-G24N)
 - 40mm Knob with internal diameter of 6mm 
 - Arduino Pro Micro
-- 2 24mm button
+- 3 24mm button
 - 2 30mm button
 - Enclosure 3D printed or any box
 
@@ -117,45 +48,13 @@ GP_5    Button0
 GP_4    Button1
 GP_15   Button2
 GP_14   Button3
+GP_6    Button4
 ```
-
 ### Flash your Arduino
 
 1. Install Aduino IDE 2.x
 2. Open Arduino_ArcadeSpinner.ino
-    1. (Optionnal) Change options in the configuration section.
+3. Install (HID-Project)[https://github.com/NicoHood/HID] library
 3. Connect your Arduino pro micro to your computer
 4. Select Arduino Leonardo with the correct com port
 5. Flash the controller
-
-## Technical informations
-In the mister source code there is two special cases
-
-If the device name start by MiSTer-S1 the device is considered as a mr.Spinner
-
-```
-// mr.Spinner
-// 0x120  - Button
-// Axis 7 - EV_REL is spinner
-// Axis 8 - EV_ABS is Paddle
-// Overlays on other existing gamepads
-if (strstr(uniq, "MiSTer-S1")) input[n].quirk = QUIRK_PDSP;
-if (strstr(input[n].name, "MiSTer-S1")) input[n].quirk = QUIRK_PDSP;
-```
-
-If the device name start by MiSTer-A1 the device is considered as an arcade stick with a spinner and/or paddle
-```
-// Arcade with spinner and/or paddle:
-// Axis 7 - EV_REL is spinner
-// Axis 8 - EV_ABS is Paddle
-// Includes other buttons and axes, works as a full featured gamepad.
-if (strstr(uniq, "MiSTer-A1")) input[n].quirk = QUIRK_PDSP_ARCADE;
-if (strstr(input[n].name, "MiSTer-A1")) input[n].quirk = QUIRK_PDSP_ARCADE;
-```
-
-## Rotary encoder
-
-The rotary encoder uses a magnet to detect rotation, it translates the orientation into pulses which are then interpreted by the arduino controller.
-
-![Img!](img/magnet.jpg "Magnet view")
-![Img!](img/magnet2.jpg "Magnet controller")
